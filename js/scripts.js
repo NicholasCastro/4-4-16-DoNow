ebb73af89f12b7d9fb6ed9d382671ec4af46914a
Nicholas Castro <10019281@sbschools.org>
2016-04-06 09:21:34 -0400

    finished mouseenter animations

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@
 $(".animate-on-mouseenter").on("mouseenter", function() {
 	$(this).toggleClass("animated flip");
 });
+
+$(".animate-on-mouseenter-2").on("mouseenter", function() {
+	$(this).toggleClass("animated fadeIn");
+});
+
+$(".animate-on-mouseenter-3").on("mouseenter", function() {
+	$(this).toggleClass("animated pulse");
+});
